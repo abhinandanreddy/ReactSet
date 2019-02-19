d95a0ca15dbbb9c94f037723af717c85a9b8e02c
abhinandanreddy <abhinandhanrdd4@gmail.com>
2019-02-19 13:04:41 -0600

    First commit, React project setup.

---
+++ src/app/index.js
@@ -0,0 +1,46 @@
+/**
+ * Created by abhi on 4/21/2017.
+ */
+
+import React from  "react";
+import {render} from "react-dom";
+import {Header}   from "./Header";
+import {Home}   from "./Home";
+
+class App extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            homelink:"initialHomeLink"
+        };
+     }
+
+     changeLinkName(newLink){
+        this.setState({
+           homelink:newLink
+        });
+     }
+    render(){
+
+        var user={ hobbies:["cricket","Coding","Surfing","swimmming"] };
+        return (
+           <div className="container">
+               <div className="row">
+                   <div className="col-xs-12">
+                        <Header link={this.state.homelink} />
+                   </div>
+               </div>
+
+               <div className="row">
+                   <div className="col-xs-12">
+                       <Home user={user} name="Abhi" age="27" changeLinkName={this.changeLinkName.bind(this)}/>
+                   </div>
+               </div>
+           </div>
+        );
+    }
+}
+
+
+render(<App/> , window.document.getElementById("root"));
